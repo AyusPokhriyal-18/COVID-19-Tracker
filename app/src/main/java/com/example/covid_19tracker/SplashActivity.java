53918ef8e6ac4e61dc032073f6dff44516ce13de
Ayush Pokhriyal <64911573+AyusPokhriyal-18@users.noreply.github.com>
2020-05-31 12:35:29 +0530

    covid19

---
+++ app/src/main/java/com/example/covid_19tracker/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.example.covid_19tracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashactivity);
+
+   new Handler().postDelayed(new Runnable() {
+       @Override
+       public void run() {
+           Intent intent=new Intent(SplashActivity.this, MainActivity.class);
+           overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out);
+      startActivity(intent);
+      finish();
+       }
+   }, 2000);
+    }
+
+
+}
+
