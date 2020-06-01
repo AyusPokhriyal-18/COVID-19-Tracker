d4fbc8d4653f1bd1d320bc1267daa1a110f735b9
Ayush Pokhriyal <64911573+AyusPokhriyal-18@users.noreply.github.com>
2020-06-01 21:06:53 +0530

    just one

--- app/src/main/java/com/example/covid_19tracker/MainActivity.java
+++ app/src/main/java/com/example/covid_19tracker/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.covid_19tracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toolbar;
+
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
+TextView t1, t2, t3;
+Toolbar toolbar;
+BottomNavigationView bottomNavigationView;
+CollapsingToolbarLayout collapsingToolbarLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activitymain2);
+
+      ViewInit()
+
+
+    }
+
+    private void ViewInit() {
+    t1=findViewById(R.id.t1);
+        t2=findViewById(R.id.t1);
+        t3=findViewById(R.id.t1);
+        toolbar= findViewById(R.id.toolbar);
+        bottomNavigationView=findViewById(R.id.bottommenu);
+        collapsingToolbarLayout=findViewById(R.id.colLayout);
+
     }
 }
