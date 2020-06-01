19dfe8395b3e1060d74006a15af9740cc3a348ec
Ayush Pokhriyal <64911573+AyusPokhriyal-18@users.noreply.github.com>
2020-06-01 19:57:11 +0530

    just one

--- app/src/main/java/com/example/covid_19tracker/MainActivity.java
+++ app/src/main/java/com/example/covid_19tracker/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activitymain);
+        setContentView(R.layout.activitymain2);
     }
 }
