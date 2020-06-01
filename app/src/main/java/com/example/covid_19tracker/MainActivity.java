04ad1e69d4a37d11cbde0b82803f517a3cf1310e
Ayush Pokhriyal <64911573+AyusPokhriyal-18@users.noreply.github.com>
2020-06-01 21:23:47 +0530

    just one

--- app/src/main/java/com/example/covid_19tracker/MainActivity.java
+++ app/src/main/java/com/example/covid_19tracker/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.covid_19tracker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.appcompat.widget.Toolbar;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toolbar;
+import android.widget.Toast;
+
 
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -21,9 +24,31 @@ CollapsingToolbarLayout collapsingToolbarLayout;
         setContentView(R.layout.activitymain2);
 
       ViewInit();
-
-
+toolbarSetup();
+bottomSetup();
     }
+
+    private void bottomSetup() {
+        bottomNavigationView.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
+            @Override
+            public void onNavigationItemReselected(@NonNull MenuItem item) {
+                switch (item.getItemId())
+                    case R.id.Home;
+            }
+        });
+    }
+
+    private void toolbarSetup() {
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Menu button Clicked",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
 
     private void ViewInit() {
     t1=findViewById(R.id.t1);
