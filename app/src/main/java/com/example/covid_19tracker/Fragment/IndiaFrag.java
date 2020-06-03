0e3c7d9e337a1328655cb253340a6b0dc10393e9
Ayush Pokhriyal <64911573+AyusPokhriyal-18@users.noreply.github.com>
2020-06-03 20:13:34 +0530

    just one

---
+++ app/src/main/java/com/example/covid_19tracker/Fragment/IndiaFrag.java
@@ -0,0 +1,29 @@
+package com.example.covid_19tracker.Fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.covid_19tracker.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class IndiaFrag extends Fragment {
+
+    public IndiaFrag() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_india, container, false);
+    }
+}
--- app/src/main/java/com/example/covid_19tracker/MainActivity.java
+++ app/src/main/java/com/example/covid_19tracker/MainActivity.java
@@ -7,7 +7,6 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
@@ -15,6 +14,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
+import com.example.covid_19tracker.Fragment.HomeFrag;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -41,7 +41,7 @@ bottomSetup();
                 switch (item.getItemId()){
                     case R.id.Home:
                      Toast.makeText(getApplicationContext(),"Home Fragment",Toast.LENGTH_SHORT).show();
-                        Fragment homeFrag= new  HomeFrag();
+                        Fragment homeFrag= new HomeFrag();
                         FragmentManager fragmentManager= getSupportFragmentManager();
 
                         FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
