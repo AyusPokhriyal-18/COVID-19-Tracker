8905e30dd7f0c7cd94c94fd7853a52e099035f77
Ayush Pokhriyal <64911573+AyusPokhriyal-18@users.noreply.github.com>
2020-07-02 21:28:48 +0530

    just one

--- app/src/main/java/com/example/covid_19tracker/SplashActivity.java
+++ app/src/main/java/com/example/covid_19tracker/SplashActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.example.covid_19tracker.Fragment.HomeFrag;
+
 public class SplashActivity extends AppCompatActivity {
 
     @Override
